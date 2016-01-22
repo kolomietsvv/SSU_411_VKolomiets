15712e5fd498fd6cb3e342faa079e48e1d6555e1
kolomietsvv <kolomiets.victoriya@gmail.com>
2016-01-22 12:31:03 +0300

    Solution changed

--- Task3/src/Solution.java
+++ Task3/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Вика on 22.01.2016.
  */
@@ -5,21 +7,69 @@ public class Solution {
     public static void main(String[] args)
     {
         Basket b=new Basket();
-        b.addProduct("Самолёт",2);
-        b.addProduct("Вертолёт",4);
-        b.addProduct("Танк",1);
-        b.addProduct("Патрон (Крутой патрон)",7000);
-        for(String prod:b.getProducts())
-            System.out.println("Продукт '" + prod + "' В количестве: " + b.getProductQuantity(prod));
-
-        b.removeProduct("Патрон (Крутой патрон)");
-        for(String prod:b.getProducts())
-            System.out.println("Продукт '" + prod + "' В количестве: " + b.getProductQuantity(prod));
-
-        b.updateProductQuantity("Самолёт",1);
-        for(String prod:b.getProducts())
-            System.out.println("Продукт '" + prod + "' В количестве: " + b.getProductQuantity(prod));
-
-        b.clear();
+        Scanner in = new Scanner(System.in);
+        String command, product, quantity;
+        System.out.println("Enter 'help' for help");
+        do {
+            System.out.print("> "); command=in.nextLine();
+            switch(command)
+            {
+                case "help":
+                    System.out.println("add");
+                    System.out.println("remove");
+                    System.out.println("update");
+                    System.out.println("clear");
+                    System.out.println("getCount");
+                    System.out.println("getAll");
+                    System.out.println("quit");
+                    break;
+                case "add":
+                    System.out.print("product name: ");
+                    product=in.nextLine();
+                    System.out.print("product quantity: ");
+                    quantity=in.nextLine();
+                    try{
+                        int count=Integer.parseInt(quantity);
+                        b.addProduct(product, count);
+                        System.out.println("Done");
+                    }
+                    catch(Exception e){System.out.println("Haven't done. Invalid input!");}
+                    break;
+                case "remove":
+                    System.out.print("product name: ");
+                    product=in.nextLine();
+                    try{b.removeProduct(product);System.out.println("Done");}
+                    catch(Exception e){System.out.println("Not done. Invalid input!");}
+                    break;
+                case "update":
+                    System.out.print("product name: ");
+                    product=in.nextLine();
+                    System.out.print("product quantity: ");
+                    quantity=in.nextLine();
+                    try{
+                        int count=Integer.parseInt(quantity);
+                        b.updateProductQuantity(product, count);
+                        System.out.println("Done");
+                    }
+                    catch(Exception e){System.out.println("Not done. Invalid input!");}
+                    break;
+                case "clear":
+                    b.clear();
+                    break;
+                case "getCount":
+                    System.out.print("product name: ");
+                    product=in.nextLine();
+                    try {System.out.println(b.getProductQuantity(product));}
+                    catch(Exception e){System.out.println("Not done. Invalid input!");}
+                    break;
+                case "getAll":
+                    for(String prd:b.getProducts())
+                        System.out.println(prd);
+                    break;
+                case"quit": break;
+                default: System.out.println("Inknown command");
+            }
+        }
+        while(!command.equals("quit"));
     }
 }
