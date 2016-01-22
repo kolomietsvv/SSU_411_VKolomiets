5f8e87b206f204ed0420bb0a52a7ae89c2733c2f
unknown <KolomiyetsVV@w12-416-26.main.sgu.ru>
2016-01-22 16:13:20 +0300

    Correacted Task1

--- Task1/src/subTask2.java
+++ Task1/src/subTask2.java
@@ -17,21 +17,25 @@ public class subTask2 {
             if(year<1800||year>2020)
                 System.out.println("Год введён не корректно (допустим ввод натуральных чисел [1800 - 2020])");
         }
-        while(year<1||year>12) {
+        while(month<1||month>12) {
             System.out.print("Введите месяц: ");
-            try{year = Integer.parseInt(in.nextLine());}
+            try{month = Integer.parseInt(in.nextLine());}
             catch (Exception e){}
-            if(year<1||year>12)
+            if(month<1||month>12)
                 System.out.println("Месяц введён не корректно (допустим ввод натуральных чисел [1 - 12])");
         }
         Calendar cal = Calendar.getInstance();
         cal.set(year,month-1,13);
-        int i=1964;
+        int i=year;
+        int sum=0;
         while(cal.compareTo(Calendar.getInstance())<0) {
-            if (cal.get(cal.DAY_OF_WEEK) == cal.FRIDAY)
+            if (cal.get(cal.DAY_OF_WEEK) == cal.FRIDAY){
                 System.out.println(cal.get(cal.YEAR));
+                sum++;
+            }
             i++;cal.set(cal.YEAR, i);
 
         }
+        System.out.println(sum);
     }
 }
